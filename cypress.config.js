3fc9a95c3519f99513fc47f9769db4561faf8198
Ali Sultani <aliakbar.su@hotmail.com>
2024-11-18 00:17:58 +0100

    Replace existing Cypress configuration with a new setup for component and end-to-end testing

---
+++ cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require("cypress");
+const { configureVisualRegression } = require("cypress-visual-regression");
+const { installPlugin } = require("@chromatic-com/cypress");
+
+module.exports = defineConfig({
+  projectId: "kkfn1n",
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    env: {
+      visualRegressionType: "regression", // pass 'base' when generating snapshots for the first time
+    },
+    screenshotsFolder: "./cypress/snapshots/actual",
+    setupNodeEvents(on) {
+      configureVisualRegression(on);
+    },
+    supportFile: false,
+  },
+  e2e: {
+    baseUrl:
+      "https://survesy-sandbox-604-preview-145578957084.us-central1.run.app",
+    setupNodeEvents(on, config) {
+      installPlugin(on, config);
+    },
+    experimentalStudio: true,
+  },
+});
