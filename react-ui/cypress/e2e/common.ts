04685ab6c66bcd2232b5ec88b0ea4edc8db155f2
Ali Sultani <aliakbar.su@hotmail.com>
2024-11-17 12:50:21 +0100

    Refactor login function to update username and password input selectors, streamline login process, and add screenshot capture

--- react-ui/cypress/e2e/common.ts
+++ react-ui/cypress/e2e/common.ts
@@ -3,12 +3,16 @@ export const login = () => {
     emailInputField.type(Cypress.env('testAccountEmail'));
     cy.contains('button', 'Login', { timeout: 2000 }).click();
     cy.origin('https://dev-8yg0ou7n4m4vg4kd.us.auth0.com', () => {
-        cy.get('#username').type(Cypress.env('testAccountEmail'));
-        cy.get('button[type="submit"][name="action"]').click();
-        cy.get('#password').type(Cypress.env('testAccountPassword'), { log: false });
+        // cy.get('#username').type(Cypress.env('testAccountEmail'));
+        // cy.get('button[type="submit"][name="action"]').click();
+        // cy.get('#password').type(Cypress.env('testAccountPassword'), { log: false });
         // cy.get('.password-toggle-label').click();
+        cy.get('input#username').type(Cypress.env('testAccountEmail'))
+        cy.contains('button[value=default]', 'Continue').click()
+        cy.get('input#password').type(Cypress.env('testAccountPassword'), { log: false })
         cy.screenshot()
-        cy.get('button[type="submit"][name="action"]').click();
+        cy.contains('button[value=default]', 'Continue').click()
+        cy.url().should('equal', 'https://survesy-sandbox-604-preview-145578957084.us-central1.run.app')
     });
 };
 
